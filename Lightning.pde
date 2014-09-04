70a4df58c6accc221637835329303c4e9558d03f
gigibyte327 <gigi.lu27@gmail.com>
2014-09-04 10:29:32 -0700

    Added Lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,54 @@
+
+int startX=(int)(Math.random()*300 + 30);
+int startY=0;
+int endX=(int)(Math.random()*300 +30);
+int endY=0;
+
 void setup()
 {
   size(300,300);
+  strokeWeight(3);
+  background(0);
 }
 void draw()
-{
+{	
+	//background(0);
+	clouds();
+	/*stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+	while(endX<300){
+		endX= startX + ((int)(Math.random()*20)-9);
+		endY= startY + ((int)(Math.random()*10));
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;
+	}
+	startX=(int)(Math.random()*300)+30;
+	startY=0;
+	endX=(int)(Math.random()*300)+30;
+	endY=0;*/
+}
+void clouds(){
+	for (int i=20; i<300; i+=65){
+		noStroke();
+		fill((int)(Math.random()*10+125),(int)(Math.random()*5+125),(int)(Math.random()*10+127));
+		ellipse(i,15,80,65);
+	}
 
 }
 void mousePressed()
 {
-
+	stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+	while(endX<300){
+		endX= startX + ((int)(Math.random()*20)-9);
+		endY= startY + ((int)(Math.random()*10));
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;
+	}
+	startX=(int)(Math.random()*300)+30;
+	startY=0;
+	endX=(int)(Math.random()*300)+30;
+	endY=0;
+	
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,58 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+
+int startX=(int)(Math.random()*300 + 30);
+int startY=0;
+int endX=(int)(Math.random()*300 +30);
+int endY=0;
+
 public void setup()
 {
   size(300,300);
-  noLoop();
+  strokeWeight(3);
+  background(0);
 }
 public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+{	
+	//background(0);
+	clouds();
+	/*stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+	while(endX<300){
+		endX= startX + ((int)(Math.random()*20)-9);
+		endY= startY + ((int)(Math.random()*10));
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;
+	}
+	startX=(int)(Math.random()*300)+30;
+	startY=0;
+	endX=(int)(Math.random()*300)+30;
+	endY=0;*/
+}
+public void clouds(){
+	for (int i=20; i<300; i+=65){
+		noStroke();
+		fill((int)(Math.random()*10+125),(int)(Math.random()*5+125),(int)(Math.random()*10+127));
+		ellipse(i,15,80,65);
+	}
+
 }
 public void mousePressed()
 {
-  redraw();
+	stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+	while(endX<300){
+		endX= startX + ((int)(Math.random()*20)-9);
+		endY= startY + ((int)(Math.random()*10));
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;
+	}
+	startX=(int)(Math.random()*300)+30;
+	startY=0;
+	endX=(int)(Math.random()*300)+30;
+	endY=0;
+	
 }
 
   static public void main(String[] passedArgs) {
